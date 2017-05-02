1970e7548693761723ac8c7766009c9567a000c9
Steffen Mauser <steffenmauser@gmx.de>
2017-05-02 16:19:51 +0200

    Lesson 3 Start

--- cookbook/src/app/app.module.ts
+++ cookbook/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeOverviewComponent } from './recipe-overview/recipe-overview.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeCreateComponent } from './recipe-create/recipe-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeOverviewComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeCreateComponent
   ],
   imports: [
     BrowserModule,
---
+++ cookbook/src/app/recipe-create/recipe-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-create',
+  templateUrl: './recipe-create.component.html',
+  styles: []
+})
+export class RecipeCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
